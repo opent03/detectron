c3360952c9e955c54e185aed2942230051115e30
viet <viet@cs.toronto.edu>
2024-09-18 12:32:38 -0400

    generic message

--- run_div.py
+++ run_div.py
@@ -18,14 +18,14 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--name', type=str, default='kl')
     parser.add_argument('--dataset', type=str, default='cifar10')
-    parser.add_argument('--loader_args', type=dict, default={'n_components':10})
+    parser.add_argument('--loader_args', type=str, default="{'n_components':10}")
     parser.add_argument('--n_runs', type=int, default=100)
     parser.add_argument('--n_perms', type=int, default=500)
     parser.add_argument('--test_size', type=int, default=50)
     parser.add_argument('--slurm_job_id', type=str, default='NA')
     parser.add_argument('--userid', type=str, default='opent03')
     args = parser.parse_args()
-    
+    args.loader_args = eval(args.loader_args)
     # Loading -----------------------------------------
     
     x_train, x_test, y_train, y_test = loader_dict[args.dataset](**args.loader_args)
